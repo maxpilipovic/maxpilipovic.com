26974bc24d64a2de634aba0ff9bdfcc5906873f4
Max Pilipovic <84630222+MaxPilipovic@users.noreply.github.com>
2024-11-21 19:24:43 -0800

    Changed navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,21 +2,12 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 const Navbar = () => (
-  <nav style={{
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: '1rem 2rem',
-    backgroundColor: '#181818',
-    color: 'white',
-  }}>
-    <h1 style={{ margin: 0 }}>
-      <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>☔️ Max Pilipovic</Link>
-    </h1>
-    <div>
-      <Link to="/notes" style={{ color: 'white', marginRight: '1rem', textDecoration: 'none' }}>Notes</Link>
-      <Link to="/projects" style={{ color: 'white', marginRight: '1rem', textDecoration: 'none' }}>Projects</Link>
-      <Link to="/aboutme" style={{ color: 'white', textDecoration: 'none' }}>About Me</Link>
+  <nav className="navbar">
+    <Link to="/" className="navbar-brand">☔️ Max Pilipovic</Link>
+    <div className="navbar-links">
+      <Link to="/notes"> Notes</Link>
+      <Link to="/projects"> Projects</Link>
+      <Link to="/aboutme">About Me</Link>
       <button id="theme-toggle" class="btn">☕</button>
     </div>
   </nav>
