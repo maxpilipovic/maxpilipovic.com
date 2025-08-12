a6ff893248d09299e224d70cadf11443ee9a4b40
maxpilipovic <maxpkusa@gmail.com>
2025-08-12 14:16:00 -0500

    Updated resume

--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from '../components/layout';
-import Resume from '../images/Max_Pilipovic_Resume2024.pdf';
+import Resume from '../images/Max-Pilipovic-Resume.pdf';
 import Seo from "../components/seo";
 
 
